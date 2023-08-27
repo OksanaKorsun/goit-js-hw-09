10e733d555ac8658432f93064377365ac6c1fe95
OksanaKorsun <oksana2081989@gmail.com>
2023-08-28 01:46:48 +0300

    lc

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-const shouldResolve = Math.random() > 0.3;
-if (shouldResolve) {
-// Fulfill
-} else {
-// Reject
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
-}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const amount = Number(form.elements.amount.value);
+  const firstDelay = Number(form.elements.delay.value);
+  const delayStep = Number(form.elements.delay.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const currentDelay = firstDelay + i * delayStep;
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
